948a4445084aec06ebbe5b779e448cfa2d8ceae5
andluis35 <and.luis35@gmail.com>
2026-04-07 11:19:01 -0300

    Adicionada refatoração no detection_model.py

--- src/models/detection_model.py
+++ src/models/detection_model.py
@@ -3,22 +3,41 @@ from sklearn.linear_model import LogisticRegression
 
 
 def load_data():
+    '''
+        Carrega o dataset de câncer de mama a partir de um arquivo CSV.
+    '''
+
     return pd.read_csv("./data/breast_cancer.csv")
 
 
 def prepare_data(dataset):
+    '''
+        Separa o dataset em variáveis independentes (X) e variável alvo (y),
+        removendo a coluna de identificação e mantendo apenas as features relevantes.
+    '''
+
     X = dataset.iloc[:, 1:-1].values
     y = dataset.iloc[:, -1].values
     return X, y
     
 
 def train_model(X, y):
+    '''
+        Treina um modelo de Regressão Logística utilizando os dados fornecidos,
+        retornando o modelo ajustado.
+    '''
+
     model = LogisticRegression(max_iter=1000)
     model.fit(X, y)
     return model
 
 
 def predict(model, input_data):
+    '''
+        Realiza a predição de uma nova amostra, retornando a 
+        classe prevista e a probabilidade associada.
+    '''
+
     pred = model.predict(input_data)[0]
     prob = model.predict_proba(input_data)[0][1]
     return pred, prob
